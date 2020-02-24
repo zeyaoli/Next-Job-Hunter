4d4c5f348e84a71521492f89bcc7179aaae558ff
zeyaoli <zeyao.li.315@gmail.com>
2020-02-24 15:20:40 -0500

    style change and fix bugs

--- api.js
+++ api.js
@@ -1,34 +1,29 @@
-// 1. fix if suggestion is 404
-// 2. fix the repetitive random words (if this is a, not let it be a again)
-// 3. style it!
+// add a loader
 let type;
 let randomWords;
 let suggestion;
+let prevWord;
 window.addEventListener('DOMContentLoaded', () => {
 	let ul = document.getElementById('parent-list');
 	let listItems = ul.getElementsByTagName('li');
 	// console.log(listItems);
 	type = 'education';
 	for (let i = 0; i < listItems.length; i++) {
-		console.log(listItems[i]);
+		// console.log(listItems[i]);
 		listItems[i].onclick = () => {
 			type = listItems[i].innerHTML;
-			console.log(type);
+			// console.log(type);
 		};
 	}
 	const getMe = document.getElementById('get-me');
 	getMe.onclick = getRandom;
 });
 
-// const getType = () => {
-// 	let e = document.getElementById('types');
-// 	type = String(e.options[e.selectedIndex].value);
-// };
-
 // async await
 const getRandom = async () => {
 	// getType();
 	// getList();
+
 	const textRan = document.getElementById('randomString');
 	const url = `http://www.boredapi.com/api/activity?type=${type}`;
 
@@ -40,27 +35,35 @@ const getRandom = async () => {
 	// console.log(words);
 	randomWords = words[Math.floor(Math.random() * words.length)];
 	// console.log(randomWords);
-	// if (prevWord == randomWords) {
-	// 	randomWords = words[Math.floor(Math.random() * words.length)];
-	// }
-	// let prevWord = randomWords;
+	if (prevWord == randomWords) {
+		randomWords = words[Math.floor(Math.random() * words.length)];
+	}
+	prevWord = randomWords;
 	// console.log(prevWord);
 
 	let jobURL = `http://api.dataatwork.org/v1/jobs/autocomplete?contains=${randomWords}`;
+	let jobResponse;
+	let jobs;
+	try {
+		jobResponse = await fetch(jobURL);
+		jobs = await jobResponse.json();
+	} catch (e) {
+		console.log('error: ', e);
+	}
 
-	let jobResponse = await fetch(jobURL);
 	console.log(jobResponse.status);
 	// need to fix this part
 	if (jobResponse.status == 404) {
-		// randomWords = words[Math.floor(Math.random() * words.length)];
 		jobURL = `http://api.dataatwork.org/v1/jobs/normalize?job_title=${randomWords}`;
-		let jobResponse = await fetch(jobURL);
-		if ((jobResponse.status = 404)) {
+		jobResponse = await fetch(jobURL);
+		jobs = await jobResponse.json();
+		if (jobResponse.status == 404) {
 			jobResponse = await fetch(`http://api.dataatwork.org/v1/jobs/unusual_titles`);
+			jobs = await jobResponse.json();
 		}
 	}
-	const jobs = await jobResponse.json();
 	const job = jobs[Math.floor(Math.random() * jobs.length)];
-	console.log(job.suggestion);
-	randomString.innerHTML = `${activity} to become a ${job.suggestion}`;
+	let description = job.suggestion || job.title;
+	// console.log(job.suggestion);
+	randomString.innerHTML = `${activity} to become a ${description}`;
 };
